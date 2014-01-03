8d86063c0ae79eb3669fa863373424fde783d138
JRGaard <jrgaard@gmail.com>
2014-01-03 13:04:32 -0800

    Add revised date_to_int8.py

--- datetime_to_int8.py
+++ datetime_to_int8.py
@@ -1,14 +1,12 @@
 import datetime, time
 
-def dateTimetoLargeInt(date):
+def dateTimetoLargeInt(year, month, day):
     seconds_in_year = 31556900
-    epoch_time_no_subseconds = int(date)
-    current_time = (seconds_in_year * (1969 - 1600) + epoch_time_no_subseconds) * 10000000
+    epoch_time_no_subseconds = int(convertDate(year, month, day))
+    LargeIntTime = (seconds_in_year * (1969 - 1600) + (epoch_time_no_subseconds - 22500)) * 10000000
 
-    print current_time
+    return LargeIntTime
 
-date = datetime.date(1988,03,21)
-date = time.mktime(date.timetuple())
-
-dateTimetoLargeInt(date)
-
+def convertDate(year, month, day):
+    date = datetime.datetime(year, month, day)
+    return time.mktime(date.timetuple())
